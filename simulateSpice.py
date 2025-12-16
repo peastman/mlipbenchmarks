ac33ce0447dbc30661d78ff2c5e3e8a13f8169b0
Peter Eastman <peastman@stanford.edu>
2025-12-16 11:28:33 -0800

    Script for simulating single molecules

--- simulateSpice.py
+++ simulateSpice.py
@@ -1,5 +1,6 @@
 import sys
 import time
+from ase import Atoms
 import ase.io
 import ase.md
 import ase.md.velocitydistribution
@@ -7,15 +8,26 @@ import ase.optimize
 import ase.units
 import torch
 import models
+import h5py
+from openff.toolkit.topology import Molecule
 
-width = sys.argv[1]
+bohr_to_A = 0.529177210903
+
+name = sys.argv[1]
 model = sys.argv[2]
-steps = int(sys.argv[3]) if len(sys.argv) > 3 else 100
-atoms = ase.io.read(f'water/water{width}.pdb')
+steps = int(sys.argv[3]) if len(sys.argv) > 3 else 200
+file = h5py.File('spice/SPICE-test.hdf5')
+group = file[name]
+smiles = group['smiles'].asstr()[0]
+mol = Molecule.from_mapped_smiles(smiles, allow_undefined_stereo=True)
+charge = int(mol.total_charge.m)
+total_atomic_number = int(sum(atom.atomic_number for atom in mol.atoms))
+spin = 1 if (charge+total_atomic_number) % 2 == 0 else 2
+atoms = Atoms(numbers=group['atomic_numbers'])
 initial_memory = torch.cuda.device_memory_used(0)
 atoms.calc = models.create_calculator(model)
-atoms.info['charge'] = 0
-atoms.info['spin'] = 1
+models.set_charge(atoms, model, charge, spin)
+atoms.set_positions(group['conformations'][0]*bohr_to_A)
 opt = ase.optimize.LBFGS(atoms)
 print('Optimizing...')
 t1 = time.time()
